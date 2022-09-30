4f19b521e38f03ea5b9af237ae983b3bd1813d81
LeviEyal <48846533+LeviEyal@users.noreply.github.com>
2022-09-30 10:21:32 +0300

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <img src="https://scontent.fsdv2-1.fna.fbcdn.net/v/t1.18169-9/11222306_134862563528319_4421213901605287296_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=Tk1Z-eXXmMUAX9mBaFR&_nc_ht=scontent.fsdv2-1.fna&oh=00_AT8JSTREzyZuIwT4r_4MELGWDPWx61cGTDOoVDVmm7gn0A&oe=635A7330" alt="" />
       <h1>משה ההומווווו באמא</h1>
-      <h2>סתם אוהב אותך</h2>
+      <h2> סתם אוהב אותך יאח</h2>
     </div>
   )
 }
